3e6cfb6bca5edb6fbcd59eb6dd45fd302626cf4d
rosmis <rosmis123@gmail.com>
2024-07-03 10:02:01 +0200

    palindrome teste

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const prompt = require("prompt");
 const date = new Date();
 const hour = date.getHours();
 
+const testIfPromptPalindrome = (str) => {
+    return str === str.split("").reverse().join("");
+};
+
 if (hour >= 6 && hour < 18) {
     console.log("Bonjour Sir Rosmis");
 } else {
@@ -11,6 +15,10 @@ if (hour >= 6 && hour < 18) {
 
 const askPrompt = () => {
     prompt.get(["data"], function (err, result) {
+        if (result.data === testIfPromptPalindrome(result.data)) {
+            console.log("Bien dit !");
+        }
+
         console.log(result.data);
         askPrompt();
     });
